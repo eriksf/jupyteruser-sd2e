e1b57d094c573d2e0d2689635a26b44429c8cb64
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2019-01-14 13:42:32 -0600

    updating singularity image based on new sd2e base

--- images/singularity/enter_singularity.sh
+++ images/singularity/enter_singularity.sh
@@ -2,6 +2,7 @@
 
 # Check to see if work defined
 if [ -n "$STOCKYARD" ]; then
+
 	# Work is defined
 	if [ -e $STOCKYARD/jupyter ]; then
 		echo "Example notebooks already detected. To refresh them, delete\n   \$STOCKYARD/jupyter"
@@ -9,29 +10,37 @@ if [ -n "$STOCKYARD" ]; then
 		echo "Unpacking example notebooks"
 		tar -xzf $SD2E_DIR/home_archive.tar.gz -C $STOCKYARD
 	fi
+
+	# Change to jupyter directory if it exists
+	echo "Changing to SD2E notebook directory"
+	cd $STOCKYARD/jupyter
+
+	# Create work and project data symlinks if they don't exist in $STOCKYARD/jupyter
+	if [ ! -e "$STOCKYARD/jupyter/tacc-work" ]; then
+		echo "Creating symlink for tacc-work"
+		ln -s ../ $STOCKYARD/jupyter/tacc-work
+	fi
+
+	if [ ! -e "$STOCKYARD/jupyter/sd2e-community" ]; then
+		echo "Creating symlink for sd2e-community"
+		ln -s /work/projects/SD2E-Community/prod/data/ $STOCKYARD/jupyter/sd2e-community
+	fi
+
+	if [ ! -e "$STOCKYARD/jupyter/sd2e-projects" ]; then
+		echo "Creating symlink for sd2e-projects"
+		ln -s /work/projects/SD2E-Community/prod/projects $STOCKYARD/jupyter/sd2e-projects
+	fi
+
+	if [ ! -e "$STOCKYARD/jupyter/sd2e-partners" ]; then
+		echo "Creating symlink for sd2e-partners"
+		ln -s /work/projects/DARPA-SD2-Partners $STOCKYARD/jupyter/sd2e-partners
+	fi
 else
-	echo "No \$STOCKYARD detected - Not unpacking example notebooks"
+	echo "No \$STOCKYARD detected - Not unpacking example notebooks or creating data symlinks"
 fi
 
 # Unpack jupyter config
 #[ ! -e $HOME/.jupyter ] && tar -xzf /usr/share/sd2e/dotjupyter.tar.gz -C $HOME
-
-# Change to jupyter directory if it exists
-if [ -n "$STOCKYARD" ]; then
-	echo "Changing to SD2E notebook directory"
-	cd $STOCKYARD/jupyter
-fi
-
-# Create work and project data symlinks if they don't exist in $STOCKYARD/jupyter
-if [ ! -e "$STOCKYARD/jupyter/tacc-work" ]; then
-	echo "Creating symlink for tacc-work"
-	ln -s ../ $STOCKYARD/jupyter/tacc-work
-fi
-
-if [ ! -e "$STOCKYARD/jupyter/sd2e-community" ]; then
-	echo "Creating symlink for sd2e-community"
-	ln -s /work/projects/SD2E-Community/prod/data/ $STOCKYARD/jupyter/sd2e-community
-fi
 
 # Delete any legacy configs
 if [ -e $HOME/.jupyter ]; then
